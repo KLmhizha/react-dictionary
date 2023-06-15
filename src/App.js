5f41ce8285358c61f83884053c86ca59999fc645
KLmhizha <132594766+KLmhizha@users.noreply.github.com>
2023-06-15 04:18:40 +0200

    Made a few changes and added my repository link

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
 			</main>
 			<footer>
 				<i className="fa-brands fa-react icon"></i> {""}
-				<span className="link">Built by Lucia Mhizha and it is open-sourced.</span>
+				<span className="link">Built by Lucia Mhizha and it is <a href="https://github.com/KLmhizha/react-dictionary.git" className="git-hub-link" target="_blank" rel="noreferrer">open-sourced.</a></span>
 			</footer>
 		</div>
 	);
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,61 +5,54 @@ import Results from "./Results";
 import { ThreeDots } from "react-loader-spinner";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
-
-
 export default function Dictionary(props) {
-let [keyword, setKeyword] = useState("polite");
-let [results, setResults] = useState(null);
-let [loaded, setLoaded] = useState(false);
+  let [keyword, setKeyword] = useState("polite");
+  let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
-function displayMeaning(response) {
-setResults(response.data[0]);
-setLoaded(true);
-}
+  function displayMeaning(response) {
+    setResults(response.data[0]);
+    setLoaded(true);
+  }
 
-function search() {
-	let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
-	axios.get(apiUrl).then(displayMeaning);
-}
+  function search() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(displayMeaning);
+  }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-function handleSubmit(event) {
-	event.preventDefault();
-	search();
-}
+  function updateMeaning(event) {
+    setKeyword(event.target.value);
+  }
 
-function updateMeaning(event) {
-	setKeyword(event.target.value);
-}
-
-if (loaded) {
-	return (
-		<div className="Dictionary">
-			<h1>Dictionary</h1>
-			<p className="paragraph">Search for a word to get the meaning.</p>
-			<section>
-				<form onSubmit={handleSubmit}>
-					<div className="row text-center">
-						<div className="col-md-6">
-						<input type="search" onChange={updateMeaning} className="form" placeholder="Type a word & press enter...."/>
-					</div>
-					</div>
-				</form>
-			</section>
-			<Results data={results}/>
-		</div>
-	);
-} else {
-	 search();
-		return (
-			<ThreeDots
-			color="#5e63b6"
-			height={100}
-			width={100}
-			ariaLabel="loading"
-			/>
-		);		
-}
-
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <h1>Dictionary</h1>
+        <p className="paragraph">Search for a word and get the meaning.</p>
+        <section>
+          <form onSubmit={handleSubmit}>
+            <div className="text-center">
+              <input
+                type="search"
+                onChange={updateMeaning}
+                className="form"
+                placeholder="Type & press enter..."
+              />
+            </div>
+          </form>
+        </section>
+        <Results data={results} />
+      </div>
+    );
+  } else {
+    search();
+    return (
+      <ThreeDots color="#5e63b6" height={100} width={100} ariaLabel="loading" />
+    );
+  }
 }
